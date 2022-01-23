ec8a2d372967885ea91a914d687caff58b1b0915
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-01-23 16:57:42 -0300

    🔨 :hammer: [Fix] Grid component without item prop
    The component which has xs prop must have item too

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { Container } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { blue } from '@mui/material/colors';
 
 import Routes from './routes/routes';
 import store from './components/store';
 import Header from './components/Header';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: blue[500],
+    },
+  },
+});
 
 const App = () => {
   const localCart = JSON.parse(localStorage.getItem('dioshopping: cart'))
@@ -14,14 +24,16 @@ const App = () => {
   }
 
   return (
-    <Provider store={store}>
-      <Container maxWidth="xl">
-        <Router>
-          <Header />
-          <Routes />
-        </Router>
-      </Container>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <Container maxWidth="xl">
+          <Router>
+            <Header />
+            <Routes />
+          </Router>
+        </Container>
+      </Provider>
+    </ThemeProvider>
   )
 }
 
